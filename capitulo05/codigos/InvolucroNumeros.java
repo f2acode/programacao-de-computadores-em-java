76bace6af639d4377fa9a75a5d7d22b85b57cc26
felipeaugustox <augustto.felipe@hotmail.com>
2017-08-31 23:54:57 -0300

    inserindo involucroNumeros

---
+++ capitulo05/codigos/InvolucroNumeros.java
@@ -0,0 +1,13 @@
+import java.io.PrintStream;
+
+public class InvolucroNumeros{
+  public static void main(String[] args){
+    PrintStream saida = System.out;
+
+    byte bt1 = Byte.parseByte("127");
+    short st1 = Short.parseShort("32767");
+    int it1 = Integer.parseInt("1234567890");
+    long lg1 = Long.parseLong("123456789012");
+    
+  }
+}
