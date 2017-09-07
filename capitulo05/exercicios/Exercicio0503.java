1911c3e1fc1f94f504e11799fe6772fbd11454a0
felipeaugustox <augustto.felipe@hotmail.com>
2017-09-06 23:53:20 -0300

    exercício 0503

---
+++ capitulo05/exercicios/Exercicio0503.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Exercicio0503{
+  public static void main(String[] args){
+
+    Scanner entrada = new Scanner(System.in);
+
+    System.out.println("Informe o seu CPF (apenas numeros):");
+    String cpf = entrada.nextLine();
+
+    cpf = cpf.substring(0, 3) + "." + cpf.substring(3, 6) + "."
+    + cpf.substring(6, 9) + "-" + cpf.substring(9, 11);
+
+    System.out.println("CPF formatado:\t" + cpf);
+
+  }
+}
