789154d7f75b343e466385fb7b46f05b50008dc1
felipeaugustox <augustto.felipe@hotmail.com>
2017-09-14 23:39:17 -0300

    inserindo exercicio capítulo 8

---
+++ capitulo08/exercicios/Exercicio0801.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Exercicio0801{
+    public static void main(String[] args){
+
+        Scanner entrada = new Scanner(System.in);
+
+        System.out.print("Informe:");
+
+    }
+}
